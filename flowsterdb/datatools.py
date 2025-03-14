6c81d3e16b3b044edcecf37f57d825955fa60849
Felix <flex89@web.de>
2025-03-14 18:50:41 +0100

    final error handling, datatools

--- flowsterdb/datatools.py
+++ flowsterdb/datatools.py
@@ -8,26 +8,41 @@ current_path = Path(__file__).parent
 en = sa.create_engine(f"sqlite:///{current_path}/FlowsterDB.db")
 
 def get_object_by_name(en: Engine, category: str, name: str) -> pd.DataFrame:
-    with en.connect() as con:
-        query = f"""
-        SELECT * FROM tbl{category} WHERE {category} LIKE '%{name}%'
-        """
-        result = pd.read_sql(query, con)
+    """Gibt es ein Ergebnis, so wird es als DataFrame zurückgegeben. Im Fehlerfall
+    wird ein RuntimeError zum Aufrufer weiter gegeben. Passt auf alle Tabellen außer 
+    tblEmployees.
+    category - Kategorie des gesuchten Objekts
+    Name - Bezeichnung des gesuchten Objekts"""
+    try:
+        with en.connect() as con:
+            query = sa.text(f"""
+            SELECT * FROM tbl{category} WHERE {category} LIKE :name""")
+            params = {'name': f"%{name}%"}
+            result = pd.read_sql(query, con=con, params=params)
         return result
+    except sa.exc.SQLAlchemyError as e:
+        raise RuntimeError("Ein Fehler in der SQL-Abfrage ist aufgetreten.") from e
+    except sa.exc.OperationalError as e:
+         raise RuntimeError("Fehler bei der Datenbankverbindung.") from e
 
 def get_employee_by_samaccountname(en: Engine, samaccountname: str) -> pd.DataFrame:
-    with en.connect() as con:
-        query = sa.text(f"""
-        SELECT * FROM tblEmployees WHERE SamAccountname like :name
-        """)
-        params = {'name': f"'%{samaccountname}%'"}
+        """Bietet die Möglichkeit nach ungefähren SamAccountNames zu suchen. 
+        Wird etwas gefunden, wird es als DataFrame zurückgegeben. Im Fehlerfall
+        wird eine Runtime Error zum Aufrufer weiter gegeben. 
+        samaccountname - AD SamAccountName des gesuchten User"""
         try:
-            result = pd.read_sql(query, con=con, params=params)
+            with en.connect() as con:
+                query = sa.text("""
+                SELECT * FROM tblEmployees WHERE SamAccountname like :name""")
+                params = {'name': f"%{samaccountname}%"}
+                result = pd.read_sql(query, con=con, params=params)
+            return result
         except sa.exc.OperationalError as e:
-            raise e
-        return result
-
+            raise RuntimeError("Fehler bei der Datenbankverbindung.") from e
+        except sa.exc.SQLAlchemyError as e:
+            raise RuntimeError("Ein Fehler in der SQL-Abfrage ist aufgetreten.") from e
+        
 
 if __name__ == '__main__':
-    print(get_employee_by_samaccountname(en, 'felix.weidemann'))
+    print(get_object_by_name(en, 'Distributor', 'potter'))
 
